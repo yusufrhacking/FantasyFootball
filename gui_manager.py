e1170dc647180201ba5bd8d4f91182e6655a9724
hackingy <yusufhacking@gmail.com>
2023-08-02 12:16:09 -0500

    Cleaned up the gui manager class a ton

--- gui_manager.py
+++ gui_manager.py
@@ -3,66 +3,79 @@ from tkinter import ttk
 
 
 class DataFrameView:
-    def __init__(self, parent, df, position_title):
-        container = ttk.Frame(parent)
-        container.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+    def __init__(self, container, df, title):
+        self.container = ttk.Frame(container)
+        self.container.grid(row=0, column=0, sticky="nsew")
 
-        header_label = ttk.Label(container, text=position_title, font=('Helvetica', 16, 'bold'))
+        self._add_header(title)
+        self._create_tree_view(df)
+        self._populate_tree_view(df)
+
+    def _add_header(self, title):
+        header_label = ttk.Label(self.container, text=title, font=('Helvetica', 16, 'bold'))
         header_label.pack(side=tk.TOP, pady=10, padx=20)
 
+    def _create_tree_view(self, df):
         columns = tuple(df.columns)
-        self.tree = ttk.Treeview(container, columns=columns, show="headings")
+        self.tree = ttk.Treeview(self.container, columns=columns, show="headings")
         self.tree.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
 
         for col in columns:
             self.tree.heading(col, text=col)
             self.tree.column(col, width=100)
 
-        scrollbar = ttk.Scrollbar(container, orient="vertical", command=self.tree.yview)
+        scrollbar = ttk.Scrollbar(self.container, orient="vertical", command=self.tree.yview)
         scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-
         self.tree.configure(yscrollcommand=scrollbar.set)
 
-        # Add data
+    def _populate_tree_view(self, df):
         for index, row in df.iterrows():
             self.tree.insert("", tk.END, values=tuple(row))
 
 
-def create_gui(root, qb_df, other_positions_df):
-    root.title("Fantasy Football Projections")
-    root.geometry("800x600")
-    root.configure(bg='lightgray')
+class FrameController:
+    def __init__(self, container, dfs, titles):
+        self.frames = {}
+        for df, title in zip(dfs, titles):
+            frame = DataFrameView(container, df, title)
+            self.frames[title] = frame.container
 
-    style = ttk.Style(root)
-    style.theme_use("clam")
-    style.configure("TFrame", background="lightgray")  # Set background color for ttk.Frame widgets
 
-    def show_frame(frame_to_show):
-        frame_to_show.tkraise()
+class ButtonController:
+    def __init__(self, root, frame_controller, button_texts):
+        button_frame = ttk.Frame(root)
+        button_frame.pack(side=tk.TOP, fill=tk.X)
+        for text in button_texts:
+            button = ttk.Button(button_frame, text=text, command=lambda f=frame_controller.frames[text]: f.tkraise())
+            button.pack(side=tk.LEFT, padx=5, pady=5)
 
-    container = ttk.Frame(root)
-    container.pack(side="top", fill="both", expand=True)
-    container.grid_rowconfigure(0, weight=1)
-    container.grid_columnconfigure(0, weight=1)
 
-    qb_frame = ttk.Frame(container)
-    other_positions_frame = ttk.Frame(container)
+class FantasyFootballApp:
+    def __init__(self, root, qb_df, other_positions_df):
+        root.title("Fantasy Football Projections")
+        root.geometry("800x600")
+        root.configure(bg='lightgray')
 
-    for frame in (qb_frame, other_positions_frame):
-        frame.grid(row=0, column=0, sticky="nsew")
+        style = ttk.Style(root)
+        style.theme_use("clam")
+        style.configure("TFrame", background="lightgray")  # Set background color for ttk.Frame widgets
 
-    DataFrameView(qb_frame, qb_df, "Quarterbacks")
-    DataFrameView(other_positions_frame, other_positions_df, "Other Positions")
+        container = ttk.Frame(root)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
-    button_frame = ttk.Frame(root)
-    button_frame.pack(side=tk.TOP, fill=tk.X)
+        frames = [qb_df, other_positions_df]
+        titles = ["Quarterbacks", "Other Positions"]
 
-    qb_button = ttk.Button(button_frame, text="Quarterbacks", command=lambda: show_frame(qb_frame))
-    qb_button.pack(side=tk.LEFT, padx=5, pady=5)
+        frame_controller = FrameController(container, frames, titles)
+        button_controller = ButtonController(root, frame_controller, titles)
 
-    other_positions_button = ttk.Button(button_frame, text="Other Positions (RB/TE/WR)",
-                                        command=lambda: show_frame(other_positions_frame))
-    other_positions_button.pack(side=tk.LEFT, padx=5, pady=5)
+        # Initially show the Quarterbacks frame
+        frame_controller.frames["Quarterbacks"].tkraise()
 
-    # Initially show the Quarterbacks frame
-    show_frame(qb_frame)
+
+# Example usage:
+# root = tk.Tk()
+# app = FantasyFootballApp(root, qb_df, other_positions_df)
+# root.mainloop()
--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import tkinter as tk
 from data_manager import get_players
-from gui_manager import create_gui
+from gui_manager import FantasyFootballApp
+
 
 def main():
     qb_df, other_positions_df = get_players()
     root = tk.Tk()
-    root.title("Top Fantasy Players")
-    root.geometry('800x600') # You can set the dimensions as per your requirement
-
-    print("Main " + str(qb_df.columns))
-
-    create_gui(root, qb_df, other_positions_df)
-
+    app = FantasyFootballApp(root, qb_df, other_positions_df)
     root.mainloop()
 
 if __name__ == "__main__":
