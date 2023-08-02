3cbbc1d85763d7bb1dca5f40f4189ae8e4e78e68
hackingy <yusufhacking@gmail.com>
2023-08-02 10:54:23 -0500

    SLightly faster now

--- gui_manager.py
+++ gui_manager.py
@@ -1,38 +1,48 @@
 import tkinter as tk
 from tkinter import ttk
+import pandas as pd
 
-def create_scrollable_frame(container, df, position_title):
-    header_label = ttk.Label(container, text=position_title, font=('Helvetica', 16, 'bold'))
-    header_label.pack(side=tk.TOP, pady=10, padx=20)
+class ScrollableFrame:
+    def __init__(self, parent, df, position_title):
+        container = ttk.Frame(parent)
+        container.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
-    canvas = tk.Canvas(container, highlightthickness=0)
-    canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+        header_label = ttk.Label(container, text=position_title, font=('Helvetica', 16, 'bold'))
+        header_label.pack(side=tk.TOP, pady=10, padx=20)
 
-    scrollbar = ttk.Scrollbar(container, orient="vertical", command=canvas.yview)
-    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.canvas = tk.Canvas(container, highlightthickness=0)
+        self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
 
-    canvas.configure(yscrollcommand=scrollbar.set)
-    canvas.bind('<Configure>', lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+        scrollbar = ttk.Scrollbar(container, orient="vertical", command=self.canvas.yview)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
-    scroll_frame = ttk.Frame(canvas)
-    canvas.create_window((0, 0), window=scroll_frame, anchor="nw")
+        self.canvas.configure(yscrollcommand=scrollbar.set)
+        self.scroll_frame = ttk.Frame(self.canvas)
+        self.canvas.create_window((0, 0), window=self.scroll_frame, anchor="nw")
 
-    # Create table header
-    header_frame = ttk.Frame(scroll_frame)
-    header_frame.pack(side=tk.TOP, fill=tk.X)
+        # Create table header
+        header_frame = ttk.Frame(self.scroll_frame)
+        header_frame.pack(side=tk.TOP, fill=tk.X)
 
-    ttk.Label(header_frame, text="Player", width=25, font=('Helvetica', 12, 'bold')).pack(side=tk.LEFT, padx=5, pady=5)
-    ttk.Label(header_frame, text="Position", font=('Helvetica', 12, 'bold')).pack(side=tk.LEFT, padx=5, pady=5)
-    ttk.Label(header_frame, text="Proj Pts", font=('Helvetica', 12, 'bold')).pack(side=tk.LEFT, padx=5, pady=5)
+        ttk.Label(header_frame, text="Player", width=25, font=('Helvetica', 12, 'bold')).pack(side=tk.LEFT, padx=5, pady=5)
+        ttk.Label(header_frame, text="Position", font=('Helvetica', 12, 'bold')).pack(side=tk.LEFT, padx=5, pady=5)
+        ttk.Label(header_frame, text="Proj Pts", font=('Helvetica', 12, 'bold')).pack(side=tk.LEFT, padx=5, pady=5)
 
-    # Add players
-    for index, row in df.iterrows():
-        row_frame = ttk.Frame(scroll_frame)
-        row_frame.pack(side=tk.TOP, fill=tk.X)
+        # Add players
+        self.rows = []
+        for index, row in df.iterrows():
+            row_frame = ttk.Frame(self.scroll_frame)
+            row_frame.pack(side=tk.TOP, fill=tk.X)
 
-        ttk.Label(row_frame, text=row['Player'], width=25).pack(side=tk.LEFT, padx=5, pady=2)
-        ttk.Label(row_frame, text=row['Position']).pack(side=tk.LEFT, padx=5, pady=2)
-        ttk.Label(row_frame, text=row['Proj Pts']).pack(side=tk.LEFT, padx=5, pady=2)
+            ttk.Label(row_frame, text=row['Player'], width=25).pack(side=tk.LEFT, padx=5, pady=2)
+            ttk.Label(row_frame, text=row['Position']).pack(side=tk.LEFT, padx=5, pady=2)
+            ttk.Label(row_frame, text=row['Proj Pts']).pack(side=tk.LEFT, padx=5, pady=2)
+
+            self.rows.append(row_frame)
+
+    def update_scrollregion(self, event):
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+
 
 def create_gui(root, qb_df, other_positions_df):
     root.title("Fantasy Football Projections")
@@ -52,5 +62,16 @@ def create_gui(root, qb_df, other_positions_df):
     notebook.add(qb_frame, text="Quarterbacks")
     notebook.add(other_positions_frame, text="Other Positions (RB/TE/WR)")
 
-    create_scrollable_frame(qb_frame, qb_df, "Quarterbacks")
-    create_scrollable_frame(other_positions_frame, other_positions_df, "Other Positions")
+    qb_scrollable_frame = ScrollableFrame(qb_frame, qb_df, "Quarterbacks")
+    other_positions_scrollable_frame = ScrollableFrame(other_positions_frame, other_positions_df, "Other Positions")
+
+    qb_scrollable_frame.canvas.bind('<Configure>', qb_scrollable_frame.update_scrollregion)
+    other_positions_scrollable_frame.canvas.bind('<Configure>', other_positions_scrollable_frame.update_scrollregion)
+
+
+# Example usage
+# qb_df = pd.DataFrame(...)
+# other_positions_df = pd.DataFrame(...)
+# root = tk.Tk()
+# create_gui(root, qb_df, other_positions_df)
+# root.mainloop()
