525102a4d6985ff681d86a84bf667959a6e93d3c
hackingy <yusufhacking@gmail.com>
2023-08-02 22:16:21 -0500

    Messed with the colors to make it look better

--- gui/draft_page.py
+++ gui/draft_page.py
@@ -5,39 +5,39 @@ from tkinter import ttk
 class DraftPageApp:
     def __init__(self, root, par_table):
         self.par_table = par_table
-        root.title("Draft Page")
+        root.title("Fantasy Draft Page")
         root.geometry("1200x800")
-        root.configure(bg='#f0f0f0')  # Soft background color to complement the row colors
+        root.configure(bg='#f0f0f0')  # Soft Gray Background
 
-        # Top Frame to house the title and brief instructions
-        top_frame = ttk.Frame(root, padding="10", style='TFrame')
+        # Top Frame for Title and Brief Instructions
+        top_frame = ttk.Frame(root, padding="10")
         top_frame.pack(side="top", fill="both", expand=False)
-        title_label = ttk.Label(top_frame, text="Fantasy Draft", font=("Helvetica", 20))
-        title_label.pack(side="left", padx=5)
-        instruction_label = ttk.Label(top_frame, text="Select players for your draft.", font=("Helvetica", 12))
-        instruction_label.pack(side="left", padx=5)
+        title_label = ttk.Label(top_frame, text="Fantasy Draft", font=("Helvetica", 24), foreground="#333333")
+        title_label.pack(side="top", padx=5)
+        instruction_label = ttk.Label(top_frame, text="Select players for your draft.", font=("Helvetica", 14), foreground="#555555")
+        instruction_label.pack(side="top", padx=5)
 
         # Bottom Frame for Treeview and Action buttons
-        bottom_frame = ttk.Frame(root, padding="10", style='TFrame')
+        bottom_frame = ttk.Frame(root, padding="10")
         bottom_frame.pack(side="top", fill="both", expand=True)
 
         # Treeview to display the PAR table
         self.tree = self.create_tree(bottom_frame)
         self.tree.grid(row=0, column=0, sticky="nsew")
 
-        # Side frame for buttons and additional controls
-        side_frame = ttk.Frame(bottom_frame, style='TFrame')
+        # Side Frame for Buttons and Additional Controls
+        side_frame = ttk.Frame(bottom_frame, padding="20")
         side_frame.grid(row=0, column=1, sticky="nsew")
 
-        # Button to add a player to the draft
+        # Button to Add a Player to the Draft
         draft_button = ttk.Button(side_frame, text="Draft Player", command=self.draft_player)
         draft_button.pack(pady=10)
 
-        # Button to remove a player from the draft
+        # Button to Remove a Player from the Draft
         remove_button = ttk.Button(side_frame, text="Remove Player", command=self.remove_player)
         remove_button.pack(pady=10)
 
-        # Resizing configuration
+        # Resizing Configuration
         bottom_frame.grid_columnconfigure(0, weight=3)
         bottom_frame.grid_columnconfigure(1, weight=1)
         bottom_frame.grid_rowconfigure(0, weight=1)
@@ -60,20 +60,27 @@ class DraftPageApp:
             tree.heading(col, text=col)
             tree.column(col, width=100)
 
-        # Define colors for different positions
-        position_colors = {
-            'QB': '#a6cee3',
-            'RB': '#1f78b4',
-            'WR': '#b2df8a',
-            'TE': '#33a02c',
-            'FLEX': '#fb9a99'
+        colors = {
+            'QB': '#1D5B79',  # Light Red
+            'RB': '#468B97',  # Light Green
+            'WR': '#EF6262',  # Light Blue
+            'TE': '#F3AA60',  # Light Orange
         }
 
-        # Insert the rows with colors based on the position
+        # colors = {
+        #     'QB': '#ED1C24',  # Vibrant Red
+        #     'RB': '#F1D302',  # Vibrant Green
+        #     'WR': '#235789',  # Vibrant Blue
+        #     'TE': '#020100',  # Vibrant Orange
+        # }
+
+        # Insert the rows with position-based colors
         for index, row in self.par_table.iterrows():
-            position = row['Position']
-            color = position_colors.get(position, 'white')  # Default to white if position is not found
-            tree.insert("", index, values=list(row), tags=(position,))
+            color = colors.get(row['Position'], '#ffffff')  # Default White
+            tree.insert("", index, values=list(row), tags=row['Position'])
+
+        # Apply the position-based colors
+        for position, color in colors.items():
             tree.tag_configure(position, background=color)
 
         # Adding a scrollbar
