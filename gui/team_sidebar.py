e0938aa6bd00053ffd22679f1b338c1db97bab41
hackingy <yusufhacking@gmail.com>
2023-08-03 13:22:34 -0500

    team sidebar looking a lot better now

--- gui/team_sidebar.py
+++ gui/team_sidebar.py
@@ -15,22 +15,23 @@ class TeamSidebar:
             label.pack(pady=2)
             self.position_labels[position] = label
 
-        self.drafted_list = tk.Listbox(parent, selectmode="single", height=10,
-                                       width=50)  # Add this if you want to display the drafted players
-        self.drafted_list.pack(pady=10)  # Add this if you want to display the drafted players
+        self.drafted_tree = ttk.Treeview(parent, columns=('Player', 'Position', 'PAR'), show='headings')
+        self.drafted_tree.heading('Player', text='Player')
+        self.drafted_tree.heading('Position', text='Position')
+        self.drafted_tree.heading('PAR', text='PAR')
+        self.drafted_tree.pack(pady=10)
 
     def add_player(self, player_data):
-        player, position, par = player_data[1], player_data[2], player_data[3]  # Adjust based on your data structure
+        player, position, par = player_data[1], player_data[2], player_data[3]
         self.drafted_players.append(player_data)
-        self.drafted_list.insert(tk.END,
-                                 f'{player} ({position}) - {par} PAR')  # Add this if you want to display the drafted players
+        self.drafted_tree.insert('', tk.END, values=(player, position, par))
         self.update_position_count(position, 1)
 
     def remove_player(self, selected_index):
         player_data = self.drafted_players[selected_index]
-        position = player_data[1]  # Assuming position is at index 1
+        position = player_data[1]
         del self.drafted_players[selected_index]
-        self.drafted_list.delete(selected_index)  # Add this if you want to display the drafted players
+        self.drafted_tree.delete(self.drafted_tree.get_children()[selected_index])
         self.update_position_count(position, -1)
 
     def update_position_count(self, position, change):
@@ -41,4 +42,5 @@ class TeamSidebar:
         self.position_labels[position].config(text=f'{position}: {new_count}/{self.position_requirements[position]}')
 
     def get_selected_index(self):
-        return self.drafted_list.curselection()[0]
+        return self.drafted_tree.selection()[0]
+
