9c0e3b04163534ad751cef90d2f3fd8796864f36
hackingy <yusufhacking@gmail.com>
2023-08-02 22:19:26 -0500

    Cleaned up the draft page app code

--- gui/draft_page.py
+++ gui/draft_page.py
@@ -9,46 +9,48 @@ class DraftPageApp:
         root.geometry("1200x800")
         root.configure(bg='#f0f0f0')  # Soft Gray Background
 
-        # Top Frame for Title and Brief Instructions
+        self.create_top_frame(root)
+        self.create_bottom_frame(root)
+
+    def create_top_frame(self, root):
         top_frame = ttk.Frame(root, padding="10")
         top_frame.pack(side="top", fill="both", expand=False)
-        title_label = ttk.Label(top_frame, text="Fantasy Draft", font=("Helvetica", 24), foreground="#333333")
+        self.add_title(top_frame)
+        self.add_instruction(top_frame)
+
+    def add_title(self, frame):
+        title_label = ttk.Label(frame, text="Fantasy Draft", font=("Helvetica", 24), foreground="#333333")
         title_label.pack(side="top", padx=5)
-        instruction_label = ttk.Label(top_frame, text="Select players for your draft.", font=("Helvetica", 14), foreground="#555555")
+
+    def add_instruction(self, frame):
+        instruction_label = ttk.Label(frame, text="Select players for your draft.", font=("Helvetica", 14),
+                                      foreground="#555555")
         instruction_label.pack(side="top", padx=5)
 
-        # Bottom Frame for Treeview and Action buttons
+    def create_bottom_frame(self, root):
         bottom_frame = ttk.Frame(root, padding="10")
         bottom_frame.pack(side="top", fill="both", expand=True)
-
-        # Treeview to display the PAR table
         self.tree = self.create_tree(bottom_frame)
         self.tree.grid(row=0, column=0, sticky="nsew")
+        self.create_side_frame(bottom_frame)
 
-        # Side Frame for Buttons and Additional Controls
-        side_frame = ttk.Frame(bottom_frame, padding="20")
-        side_frame.grid(row=0, column=1, sticky="nsew")
-
-        # Button to Add a Player to the Draft
-        draft_button = ttk.Button(side_frame, text="Draft Player", command=self.draft_player)
-        draft_button.pack(pady=10)
-
-        # Button to Remove a Player from the Draft
-        remove_button = ttk.Button(side_frame, text="Remove Player", command=self.remove_player)
-        remove_button.pack(pady=10)
-
-        # Resizing Configuration
         bottom_frame.grid_columnconfigure(0, weight=3)
         bottom_frame.grid_columnconfigure(1, weight=1)
         bottom_frame.grid_rowconfigure(0, weight=1)
 
-        # Styling
-        style = ttk.Style()
-        style.theme_use('default')
-        style.configure('TFrame', background='#f0f0f0')  # Match frame background with root background
-        style.configure('TButton', background='#d9d9d9')  # Soft color for buttons
+    def create_side_frame(self, bottom_frame):
+        side_frame = ttk.Frame(bottom_frame, padding="20")
+        side_frame.grid(row=0, column=1, sticky="nsew")
+        self.create_draft_button(side_frame)
+        self.create_remove_button(side_frame)
 
-    # Rest of your code (create_tree, draft_player, remove_player)
+    def create_draft_button(self, side_frame):
+        draft_button = ttk.Button(side_frame, text="Draft Player", command=self.draft_player)
+        draft_button.pack(pady=10)
+
+    def create_remove_button(self, side_frame):
+        remove_button = ttk.Button(side_frame, text="Remove Player", command=self.remove_player)
+        remove_button.pack(pady=10)
 
     def create_tree(self, root):
         tree = ttk.Treeview(root, selectmode="browse")
@@ -66,14 +68,6 @@ class DraftPageApp:
             'WR': '#EF6262',  # Light Blue
             'TE': '#F3AA60',  # Light Orange
         }
-
-        # colors = {
-        #     'QB': '#ED1C24',  # Vibrant Red
-        #     'RB': '#F1D302',  # Vibrant Green
-        #     'WR': '#235789',  # Vibrant Blue
-        #     'TE': '#020100',  # Vibrant Orange
-        # }
-
         # Insert the rows with position-based colors
         for index, row in self.par_table.iterrows():
             color = colors.get(row['Position'], '#ffffff')  # Default White
