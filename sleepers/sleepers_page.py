10f57cd4384788356cfb7d2bf471ed4c13186cbe
hackingy <yusufhacking@gmail.com>
2023-08-04 16:18:40 -0500

    cleaned up the sleepers page app, and I just need to integrate the player view across everything else now

--- sleepers/sleepers_page.py
+++ sleepers/sleepers_page.py
@@ -20,7 +20,6 @@ class SleepersPageApp:
 
         self.create_top_frame(root)
         player_df_view = PlayerDFView(root, self.data_table)
-        # self.create_bottom_frame(root)
 
     def create_top_frame(self, root):
         top_frame = ttk.Frame(root, padding="10")
@@ -30,39 +29,3 @@ class SleepersPageApp:
     def add_title(self, frame):
         title_label = ttk.Label(frame, text="Sleepers", font=("Helvetica", 24), foreground="#333333")
         title_label.pack(side="top", padx=5)
-
-    def create_bottom_frame(self, root):
-        self.bottom_frame = ttk.Frame(root, padding="10")
-        self.bottom_frame.pack(side="top", fill="both", expand=True)
-        self.tree = self.create_tree(self.bottom_frame)
-        self.tree.grid(row=0, column=0, sticky="nsew")
-
-        self.bottom_frame.grid_columnconfigure(0, weight=3)
-        self.bottom_frame.grid_columnconfigure(1, weight=1)
-        self.bottom_frame.grid_rowconfigure(0, weight=1)
-
-
-    def create_tree(self, root):
-        tree = ttk.Treeview(root, selectmode="browse")
-        tree["columns"] = list(self.data_table.columns)
-        tree["show"] = "headings"
-
-        self.create_columns(tree)
-        self.insert_rows(tree)
-        self.add_scrollbar(tree, root)
-
-        return tree
-
-    def create_columns(self, tree):
-        for col in self.data_table.columns:
-            tree.heading(col, text=col)
-            tree.column(col, width=100)
-
-    def insert_rows(self, tree):
-        for index, row in self.data_table.iterrows():
-            tree.insert("", index, values=list(row), tags=row['Position'])
-
-    def add_scrollbar(self, tree, root):
-        scrollbar = ttk.Scrollbar(root, orient="vertical", command=tree.yview)
-        scrollbar.grid(row=0, column=1, sticky='ns')
-        tree.configure(yscrollcommand=scrollbar.set)
